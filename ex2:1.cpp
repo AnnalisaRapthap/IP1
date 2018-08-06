3d9f5d6c6441c6a5daf6162640abeeb0ded45361
AnnalisaRapthap <31178516+AnnalisaRapthap@users.noreply.github.com>
2018-08-06 17:54:58 +0530

    Create ex2:1.cpp

---
+++ ex2:1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float g,cf;
+	cout << "Enter the number of gallons:";
+	cin >> g;
+	cout << g <<endl;
+	cf=7.481*g;
+	cout << "The amount in cubic feet=" <<" "  << cf;
+	
+	
+}
