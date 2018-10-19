b4c60f0e926b43e80539d747f9efd58ba83f846e
Annalisa <31178516+AnnalisaRapthap@users.noreply.github.com>
2018-10-19 22:34:47 +0530

    Add files via upload

--- person.cpp
+++ person.cpp
@@ -1,58 +1,63 @@
 #include<iostream>
 using namespace std;
-class person
+class Person
 {
-	protected:
-	char name[70];
-	int age;
-	int salary;
-	int rn;
+   protected:
+   char name[40];
+   int age;
+   public:
+	void getdata()
+	{
+	cout << "\nEnter the name: ";
+	cin >> name;
+	cout << "\nEnter the age: ";
+	cin>> age;
+	}
+	void putdata()
+	{
+		cout << "\nName: " << name;
+		cout << "\nAge: " << age;
+    }
 };
-class teacher : public person
+class Student :public Person
 {
+	int roll;
 	public:
 	void getdata()
 	{
-	    cout << "Enter the name of Teacher: ";
-		cin >> name;
-		cout << "Enter the salaray of the Teacher: ";
-		cin >> salary;
+		Person :: getdata();
+		cout << "\nEnter the roll no: ";
+		cin >> roll; 
 	}
-	void showdata()
+	void putdata()
 	{
-	    cout<<"Name of teacher: " << name;
-		cout << "\nSalary of teacher: " << salary << endl;
+		Person :: putdata();
+		cout << "\nroll no: " << roll;
 	}
 };
-class student: public person
+class Teacher :public Person
 {
+	int sal;
 	public:
 	void getdata()
 	{
-		cout << "Enter the name of the student: ";
-		cin >> name;
-		cout << "Enter the roll no. of the student: ";
-		cin >> rn;
+		Person :: getdata();
+		cout << "\nEnter the salary: ";
+		cin >> sal; 
 	}
-		void showdata()
+	void putdata()
 	{
-	    cout<<"Name of Student: " << name;
-		cout << "\nRoll no of student: " << rn << endl;
+		Person :: putdata();
+		cout << "\nSalary: " << sal;
 	}
 };
 int main()
 {
-	teacher t1,t2,t3;
-	student s1,s2,s3;
-	t1.getdata();
-	t1.showdata();
-	t2.getdata();
-	t2.showdata();
-
-	s1.getdata();
-	s1.showdata();
-	s2.getdata();
-	s2.showdata();
-
+	Student s;
+	Teacher t;
+	s.getdata();
+	s.putdata();
+	t.getdata();
+	t.putdata();
 	
 }
